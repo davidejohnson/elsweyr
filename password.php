08c2583cfaf7a55fa2731c821469e2a9cf65fbe3
David Johnson <david@fruitfulsuccess.co.uk>
2018-05-23 12:06:12 +0100

    Initial commit

---
+++ password.php
@@ -0,0 +1,3 @@
+<?php
+    $data = array ('error'=>true, 'url'=>'');
+    echo json_encode($data);
---
+++ x.js
@@ -0,0 +1,15 @@
+var d = function(str){document.writeln(str)};
+
+d('<html><head>'+
+  '<title>Elsweyr</ti'+
+  'tle><li'+
+  'nk rel="stylesheet" ty'+
+  'pe="text/css" href="termi'+
+  'nal.css" media="screen" /><me'+
+  'ta name="viewport" content="height=device-height,width=device-width, initial-scale=1, maximum-scale=1"><b'+
+  'ody><article id="terminal-container">Welcome to QuantumPACS (&trade;) ECS Operating System, Version v6.4 on node LON2 <br/>Last interactive login on Monday, 13-JUL-1987 03:41<br/>Username: System');
+d('<div id="visible-sections"></div><div id="command-line"><span>Password:</span><div class="commands"><b> </b></div></div></article>');
+d('<scri'+
+  'pt src="term'+
+  'inal.js"></scr'+
+  'ipt></body></html?');
